51e6f36a97aa82c19a990f577ef5b2f303eebd46
Valber <valber_558@hotmail.com>
2024-03-01 15:44:37 -0300

    Requisito-02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
 
+import pytest
 
 def test_encrypt_message():
-    pass
+
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "key")
+
+
+    assert encrypt_message("hello", 0) == "olleh"
+    assert encrypt_message("hello", 1) == "h_olle"
+    assert encrypt_message("hello", 2) == "oll_eh"
+    assert encrypt_message("hello", 3) == "leh_ol"
+
