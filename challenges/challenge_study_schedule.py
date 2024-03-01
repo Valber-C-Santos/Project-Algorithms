a113d583f95aaa5307d228a2df5c1f5118e5f11c
Valber <valber_558@hotmail.com>
2024-03-01 15:14:20 -0300

    Requisito-1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
-def study_schedule(permanence_period, target_time):
-    """Iniciando Projeto"""
-    raise NotImplementedError
+def study_schedule(presence_periods, target_time):
+    if target_time is None:
+        return None
+
+    if not all(isinstance(period, tuple) and len(period) == 2 and 
+               isinstance(period[0], int) and isinstance(period[1], int) 
+               for period in presence_periods):
+        return None
+
+    students_count = 0
+    
+    for start, end in presence_periods:
+        if start <= target_time <= end:
+            students_count += 1
+
+    return students_count
