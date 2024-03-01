1e7e1b98f0c67849740996a134d5ee7892772df3
Valber <valber_558@hotmail.com>
2024-03-01 16:38:09 -0300

    Requisito-04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,37 @@
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+    return result
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left_half = arr[:middle]
+    right_half = arr[middle:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if first_string == "" and second_string == "":
+        return "", "", False
+    
+    word1_sorted = merge_sort(first_string.lower())
+    word2_sorted = merge_sort(second_string.lower())
+
+    return "".join(word1_sorted), "".join(word2_sorted), word1_sorted == word2_sorted
